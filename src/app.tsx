89fa120473ba9ee5fbcbec7eb52d1c453440014b
Dmitry <ggbox@mail.ru>
2023-03-22 11:12:17 +0400

    fix img import type

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
+import img from "./assets/img/postcard.png";
 
 import './app.scss'
 
 const App: React.FC = () => {
-  return <h2>Hi, bitch</h2>;
+  return (
+    <>
+      <h2>Hi, bitch</h2>
+      <img src={img} alt="img" />
+    </>);
 };
 
 export default App;
